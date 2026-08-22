1a692de2d708844c57a3ce6939389af8c8a77072
tunder00 <kalyanbn19@gmail.com>
2026-08-22 23:32:26 +0530

    LL pop 1st element

---
+++ linked_list/LL_pop1st_10.py
@@ -0,0 +1,82 @@
+# pop 1st means removing element from the 1st of linked list 
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def printList(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.length == 0 :
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def popFirst(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+            # notice here we do not have self.head = None
+            # because temp.next = None this line before will take care of that.
+        return temp
+
+ll = LinkedList(1)
+ll.append(2)
+ll.append(3)
+ll.printList()
+print("--------------")
+print(ll.pop().value,"end value popped")
+print("--------------")
+ll.printList()
+print("--------------")
+print(ll.popFirst().value,"1st value popped")
+print("--------------")
+ll.printList()
